5a220b04260d25a9e41b02c4f380be7463c35129
TimofeyGoryaynov <75072701+TimofeyGoryaynov@users.noreply.github.com>
2021-09-06 13:45:06 +0300

    Add files via upload

---
+++ Code/Lab1.dpr
@@ -0,0 +1,16 @@
+program Example1;
+{$APPTYPE CONSOLE}
+uses  SysUtils;
+Var A,B,C,D,E,X1,X2:Single;
+Begin WriteLn('Input A, B, C:');
+  	ReadLn(A,B,C);
+  	D:= Sqr(B)- 4*A*C;
+  	if D>=0 then
+   	   	begin E:=2*A;
+     			 X1:= (-B+Sqrt(D))/E;X2:= (-B-Sqrt(D))/E;
+      		 WriteLn('X1=', X1:6:1, '  X2=', X2:6:1)
+    	   	end
+      else WriteLn('No result');
+ 	ReadLn;
+end.
+
